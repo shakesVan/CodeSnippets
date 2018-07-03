8edc77292f9dfd8a7da8910670e699dd5806d11e
fanyao <fanyao@manqian.cn>
2018-07-03 10:30:34 +0800

    add backup

--- README.md
+++ README.md
@@ -6,3 +6,7 @@ git clone git@github.com:xiaoyao250/CodeSnippets.git
 cd CodeSnippets/
 ./copy.sh
 ```
+
+###tips
+代码块的地址在~/Library/Developer/Xcode/UserData/CodeSnippets
+执行copy之前会自动备份本地的，备份路径为~/Library/Developer/Xcode/UserData/CodeSnippetsBackup
--- copy.sh
+++ copy.sh
@@ -2,5 +2,6 @@
 #
 #  copy CodeSnippets to My CodeSnippets
 #
-
+mkdir ../CodeSnippetsBackup
+sudo cp -R ~/Library/Developer/Xcode/UserData/CodeSnippets/* ~/Library/Developer/Xcode/UserData/CodeSnippetsBackup/
 sudo cp -R ./* ~/Library/Developer/Xcode/UserData/CodeSnippets/
